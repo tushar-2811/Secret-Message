b8f58fc87c684d77b95007370f928a52250e915b
Tushar Rawat <tusharrawat52@gmail.com>
2024-07-10 23:35:24 +0530

    add get accepting message status api

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -4,6 +4,8 @@ import UserModel from "@/model/User";
 import dbConnect from "@/lib/db";
 import { User } from "next-auth";
 
+
+// toggle the accepting messages status
 export async function POST(request: Request){
       await dbConnect();
       try {
@@ -48,6 +50,57 @@ export async function POST(request: Request){
             {
                 success : false,
                 message : "failed to update user status for accepting messages"
+            },{status : 500}
+        )
+      }
+}
+
+
+
+// checking if current user is accepting messages or not
+export async function GET(request: Request) {
+       await dbConnect();
+      try {
+        const session = await getServerSession(authOptions);
+        const user = session?.user as User;
+
+        if(!session || !session.user){
+            return Response.json(
+                {
+                    success : false,
+                    message : "Not Authenticated"
+                },{status : 401}
+            )
+        }
+
+        const userId = user._id;
+        
+
+        const existingUser = await UserModel.findById(userId);
+
+        if(!existingUser){
+            return Response.json(
+                {
+                    success : false,
+                    message : "user not found"
+                },{status : 404}
+            )
+        }
+
+        return Response.json(
+            {
+                success : true,
+                isAcceptingMessages : existingUser.isAcceptingMessage,
+                message : "Message acceptance status "
+            },{status : 201}
+        )
+    
+      } catch (error) {
+        console.log("failed to get user status for accepting messages" , error);
+        return Response.json(
+            {
+                success : false,
+                message : "failed to get user status for accepting messages"
             },{status : 500}
         )
       }
