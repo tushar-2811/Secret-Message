416056c1efab6e69480609cb5287cd27a40252c0
Tushar Rawat <tusharrawat@Tushars-MacBook-Air.local>
2024-07-17 00:03:43 +0530

    fix test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
      <>
      <h1>Hello world</h1>
-     <h2>Welcome to the secret-message app</h2>
+     <h2>Welcome to the secret-message app , test</h2>
      </>
   );
 }
